48014c9f55f004cbe1024e6b87f83984e2c13e8d
Andero <andero.sc@gmail.com>
2025-08-19 12:45:43 +0300

    Added global state variable. Moved raceControl and NextRace to separate files. Small changes in front-desk to make state.js work

---
+++ backend/state.js
@@ -0,0 +1,38 @@
+// state.js
+export let raceTrackState = {
+  //Upcoming races -> pushed from front desk(?)
+  upComingRaces: [
+    {
+      sessionName: 'race1',
+      drivers: [
+        { 
+          name: 'Milvi', 
+          carNumber: 1, 
+          fastestLap: null, 
+          currentLap: 0 },
+        { 
+          name: 'Endel', 
+          carNumber: 2, 
+          fastestLap: null, 
+          currentLap: 0 },
+      ],
+      durationSeconds: 60,
+    },
+  ],
+
+  //Race Control takes the first element of upComingRaces and assignes its values to currentRace. Then removes 
+  // first element of upComingRaces
+  currentRace: {
+    sessionName: null,
+    startTime: null,
+    endTime: null,
+    drivers: [],
+    durationSeconds: null,
+    raceMode: 'Safe',
+  },
+  //When current race ends, it will be pushed to history.
+  raceHistory: [
+    
+  ]
+}
+
