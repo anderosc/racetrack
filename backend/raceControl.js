9a930f427dc1a2cf64a48a42e9e68146a9a82506
Andero <andero.sc@gmail.com>
2025-08-22 14:10:27 +0300

    Better user flow on race control page

--- backend/raceControl.js
+++ backend/raceControl.js
@@ -4,35 +4,47 @@ export function raceControl(io, socket){
 
   let timerInterval = null;
 
-  socket.on("race:init", (raceTrackState) =>{
-    if(!raceTrackState.currentRace?.sessionName){
-      if(!raceTrackState.upComingRaces?.[0]?.sessionName){
+  socket.on("race:init", () =>{
+    socket.emit("race:init:update", raceTrackState)
+  });
 
-      } else {
-      io.emit("race:update", assignNextRide())
-      }
-    }
-  })
 
+  socket.on("race:getState", (_, callback) => {
+    // if(!raceTrackState.currentRace?.sessionName){
+    //   if(!raceTrackState.upComingRaces?.[0]?.sessionName){
+    //       return;
+    //   } 
+    // }
+    callback(raceTrackState); 
+    // console.log(raceTrackState)
+  });
 
   socket.on('race:start', () => {
   // Timer loop - updates every second. 60 second development
   // TODO -> 10min for "npm run dev" and 60second for "npm start"
+  assignNextRace()
+  // console.log(state)
+  io.emit("race:update", raceTrackState)
   raceTrackState.currentRace.raceMode = "Safe"
+  raceTrackState.currentRace.isStarted = true;
+
   timerInterval = setInterval(() => {
+
     if (!raceTrackState.currentRace?.durationSeconds){
       return;
     }
 
     //Countdown seconds
   raceTrackState.currentRace.durationSeconds = raceTrackState.currentRace.durationSeconds - 1;
+  // console.log(raceTrackState.currentRace.durationSeconds)
 
-    //If timer reaches 0 then stop timer and set race mode "Finish"
+    //If timer reaches 0 then stop timer
   if (raceTrackState.currentRace.durationSeconds <= 0) {
     clearInterval(timerInterval);
     timerInterval = null;
     raceTrackState.currentRace.durationSeconds = 0;
     raceTrackState.currentRace.raceMode = "Finish";
+    io.emit("race:finish", raceTrackState)
   }
 
   }, 1000);
@@ -55,8 +67,9 @@ export function raceControl(io, socket){
 
   // End session for all.
   socket.on("race:endSession", () => {
-    raceTrackState.raceHistory.push(raceTrackState.currentRace);
-    raceTrackState.currentRace = null;
+    raceTrackState.currentRace.raceMode = "Danger"
+    // raceTrackState.raceHistory.push(raceTrackState.currentRace);
+    raceTrackState.currentRace.isEnded = true;
     io.emit("state:update",  raceTrackState);
   })
 
@@ -73,9 +86,10 @@ export function raceControl(io, socket){
 }
 
 
-function assignNextRide(){
+ function assignNextRace(){
+  // console.log("what is happening")
   if (!raceTrackState.upComingRaces?.[0]?.sessionName){
-      // console.log(raceTrackState.upComingRaces[0].sessionName)
+      // console.log("sessionname: " , raceTrackState.upComingRaces[0].sessionName)
     return;
   }
       //Assign next race values to global currentRace variable.
@@ -87,7 +101,10 @@ function assignNextRide(){
       durationSeconds: raceTrackState.upComingRaces[0].durationSeconds,
       raceMode: null,
     }
-    
+    // console.log(raceTrackState.currentRace)
   //Remove first element fromt upComingRace
+  //TODO: IF it is last element, delete it.
   raceTrackState.upComingRaces.shift();
 }
+
+
--- backend/state.js
+++ backend/state.js
@@ -2,33 +2,57 @@
 export let raceTrackState = {
   //Upcoming races -> pushed from front desk(?)
   upComingRaces: [
-    {
-      sessionName: 'race1',
-      drivers: [
-        { 
-          name: 'Milvi', 
-          carNumber: 1, 
-          fastestLap: null, 
-          currentLap: 0 },
-        { 
-          name: 'Endel', 
-          carNumber: 2, 
-          fastestLap: null, 
-          currentLap: 0 },
-      ],
-      durationSeconds: 60,
-    },
+    // {
+    //   sessionName: 'race1',
+    //   drivers: [
+    //     { 
+    //       name: 'Milvi', 
+    //       carNumber: 1, 
+    //       fastestLap: null, 
+    //       currentLap: 0 },
+    //     { 
+    //       name: 'Endel', 
+    //       carNumber: 2, 
+    //       fastestLap: null, 
+    //       currentLap: 0 },
+    //   ],
+    //   durationSeconds: 60,
+    //   raceMode: "",
+    //   isStarted : false,
+    //   isEnded : false,
+    // },
+    // {
+    //   sessionName: 'race2',
+    //   drivers: [
+    //     { 
+    //       name: 'Milvikas', 
+    //       carNumber: 1, 
+    //       fastestLap: null, 
+    //       currentLap: 0 },
+    //     { 
+    //       name: 'Endelhehe', 
+    //       carNumber: 2, 
+    //       fastestLap: null, 
+    //       currentLap: 0 },
+    //   ],
+    //   durationSeconds: 60,
+    //   raceMode: "",
+    //   isStarted : false,
+    //   isEnded : false,
+    // },
   ],
 
   //Race Control takes the first element of upComingRaces and assignes its values to currentRace. Then removes 
   // first element of upComingRaces
   currentRace: {
-    sessionName: null,
-    startTime: null,
+    sessionName: "",
+    startTime: "",
     endTime: null,
     drivers: [],
-    durationSeconds: null,
-    raceMode: null,
+    durationSeconds: 0,
+    raceMode: "",
+    isStarted: false,
+    isEnded : false,
   },
   //When current race ends, it will be pushed to history.
   raceHistory: [
