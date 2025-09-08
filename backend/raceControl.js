15f5265bbc047d0ef5d42b090b26c7b9f264dd29
Andero <andero.sc@gmail.com>
2025-09-08 11:06:57 +0300

    Added error handling for starting new race -> if there is no drivers assigned, then you can't start it

--- backend/raceControl.js
+++ backend/raceControl.js
@@ -10,14 +10,10 @@ export function raceControl(io, socket){
     //"Immediately Invoked Function Expression"
     // Check before if server was closed before and do we have current race saved data. If yes, continue it.
   (function startRaceIfOngoing() {
-    console.log(raceTrackState)
 
     if (!timerInterval && raceTrackState.currentRace.isStarted && !raceTrackState.currentRace.isEnded) {
-      console.log(raceTrackState.currentRace)
-        // console.log("Found it");
         timerInterval = setInterval(() => {
             raceTrackState.currentRace.durationSeconds -= 1;
-            console.log(raceTrackState)
             // If timer reaches 0
             if (raceTrackState.currentRace.durationSeconds <= 0) {
                 clearInterval(timerInterval);
@@ -25,70 +21,51 @@ export function raceControl(io, socket){
                 raceTrackState.currentRace.durationSeconds = 0;
                 raceTrackState.currentRace.raceMode = "Finish";
             }
-
             saveState();
-            
         }, 1000);
     }
 })();
-
-
-    
   
 
   socket.on("race:init", () =>{
     socket.emit("race:init:update", raceTrackState)
   });
 
-
-  socket.on("race:getState", (_, callback) => {
-    // if(!raceTrackState.currentRace?.sessionName){
-    //   if(!raceTrackState.upComingRaces?.[0]?.sessionName){
-    //       return;
-    //   } 
-    // }
-    callback(raceTrackState); 
-    // console.log(raceTrackState)
-  });
-
   socket.on('race:start', () => {
-  // Timer loop - updates every second. 60 second development
-  // TODO -> 10min for "npm run dev" and 60second for "npm start"
-  assignNextRace()
-  saveState();
-  // console.log(state)
-  raceTrackState.currentRace.raceMode = "Safe"
-  raceTrackState.currentRace.isStarted = true;
-  raceTrackState.currentRace.isEnded = false;
-  raceTrackState.currentRace.durationSeconds = TIMER_DURATION;
-  io.emit("race:update", raceTrackState)
+  // Timer loop - updates every second. 60 second development, 600 for production
 
-
-
-
-  timerInterval = setInterval(() => {
-
-    if (!raceTrackState.currentRace?.durationSeconds){
-      return;
-    }
-
-    //Countdown seconds
-  raceTrackState.currentRace.durationSeconds = raceTrackState.currentRace.durationSeconds - 1;
-  // console.log(raceTrackState.currentRace.durationSeconds)
-  console.log(raceTrackState);
-
-
-    //If timer reaches 0 then stop timer
-  if (raceTrackState.currentRace.durationSeconds <= 0) {
-    clearInterval(timerInterval);
-    timerInterval = null;
-    raceTrackState.currentRace.durationSeconds = 0;
-    raceTrackState.currentRace.raceMode = "Finish";
-    io.emit("race:finish", raceTrackState)
+  if(raceTrackState.upComingRaces[0].drivers.length === 0){
+    socket.emit("race:error", "Drivers not assigned to next race");
+    return;
   }
-  saveState();
-  }, 1000);
-  io.emit("state:update",  raceTrackState );
+    assignNextRace()
+    saveState();
+    // console.log(state)
+    raceTrackState.currentRace.raceMode = "Safe"
+    raceTrackState.currentRace.isStarted = true;
+    raceTrackState.currentRace.isEnded = false;
+    raceTrackState.currentRace.durationSeconds = TIMER_DURATION;
+    io.emit("race:update", raceTrackState)
+
+    timerInterval = setInterval(() => {
+
+      if (!raceTrackState.currentRace?.durationSeconds){
+        return;
+      }
+
+      //Countdown seconds
+    raceTrackState.currentRace.durationSeconds = raceTrackState.currentRace.durationSeconds - 1;
+      //If timer reaches 0 then stop timer
+      if (raceTrackState.currentRace.durationSeconds <= 0) {
+        clearInterval(timerInterval);
+        timerInterval = null;
+        raceTrackState.currentRace.durationSeconds = 0;
+        raceTrackState.currentRace.raceMode = "Finish";
+        io.emit("race:finish", raceTrackState)
+      }
+      saveState();
+      }, 1000);
+      io.emit("state:update",  raceTrackState );
   });
 
   //Change modes, update them
@@ -142,9 +119,7 @@ export function raceControl(io, socket){
       durationSeconds: raceTrackState.upComingRaces[0].durationSeconds,
       raceMode: null,
     }
-    // console.log(raceTrackState.currentRace)
   //Remove first element fromt upComingRace
-  //TODO: IF it is last element, delete it.
   raceTrackState.upComingRaces.shift();
 }
 
