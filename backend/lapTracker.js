e1dc3e094078d542eeef1349cbd546f7c52adf18
Rainer Lestal <rainer.lestal@gmail.com>
2025-09-14 19:02:40 +0300

    leaderboard fixes

--- backend/lapTracker.js
+++ backend/lapTracker.js
@@ -1,30 +1,67 @@
 
 
-const drivers = {};
+let activeDrivers = {};
+let lastRaceName = null;
 
-export function handleLapTracking(io, socket) {
-    socket.on('lap:completed', (data) => {
-        const driver = data.driver;
-        const now = Date.now();
+export function handleLapTracking(io, socket, raceTrackState) {
 
-        if (!drivers[driver]) {
-            drivers[driver] = {
-                lapsCompleted: 0,
-                lastTime: now,
-                lapTimes: []
-            };
+    // reset drivers when new race starts or leaderboard request update
+    function ensureActiveDriversReset() {
+        const currentRace = raceTrackState.currentRace;
+        if (!currentRace) return;
+
+        if (lastRaceName !== currentRace.sessionName) {
+            activeDrivers = {}
+            lastRaceName = currentRace.sessionName;
+
+            // initialize all race drivers with 0 laps
+            currentRace.drivers.forEach(driver => {
+                activeDrivers[driver.carNumber] = {
+                    lapsCompleted: 0,
+                    lastTime: null,
+                    lapTimes: []
+                };
+            });
+            // emit initial empty leaderboard
+            io.emit('leaderboard:update', activeDrivers);
         }
+    }
 
-        let lapTime = null;
-        if (drivers[driver].lapsCompleted > 0) {
-            lapTime = now - drivers[driver].lastTime;
-            drivers[driver].lapTimes.push(lapTime);
-        }
-
-        drivers[driver].lapsCompleted++;
-        drivers[driver].lastTime = now;
-
-        io.emit('lap:completed', { driver, lap: drivers[driver].lapsCompleted, lapTime });
-        io.emit('leaderboard:update', drivers);
+    socket.on('state:update', () => {
+        ensureActiveDriversReset();
     });
+
+
+    socket.on('lap:completed', ({driver}) => {
+        ensureActiveDriversReset();
+
+        const now = Date.now();
+        const record = activeDrivers[driver];
+
+        if (!record) return;
+
+        if (record.lastTime == null) {
+            // first click set start time
+            record.lastTime = now;
+            return;
+        }
+
+        const lapTime = now - record.lastTime;
+        record.lapTimes.push(lapTime);
+        record.lapsCompleted++;
+        record.lastTime = now;
+
+
+        io.emit('lap:completed', {
+            driver,
+            lap: record.lapsCompleted,
+            lapTime
+        });
+        io.emit('leaderboard:update', activeDrivers);
+    });
+
+    socket.on('leaderboard:request', () => {
+        ensureActiveDriversReset();
+        socket.emit('leaderboard:update', activeDrivers);
+    })
 }
--- frontend/js/private/lapLineTracker.js
+++ frontend/js/private/lapLineTracker.js
@@ -2,8 +2,13 @@ const socket = io();
 
 const container = document.getElementById("buttonsContainer");
 
+let currentRaceName = null;
+
 // dynamically create buttons based on currentRace drivers
-function renderCarButtons(drivers) {
+function renderCarButtonsOnce(drivers, raceName) {
+    if (currentRaceName === raceName) return;
+    currentRaceName = raceName;
+
     container.innerHTML = '';
     drivers.forEach(driver => {
         const btn = document.createElement("button");
@@ -20,16 +25,40 @@ function completeLap(carNumber) {
     socket.emit('lap:completed', { driver: carNumber });
 }
 
+// handle disabling buttons when race finishes
+function disableButtons() {
+    document.querySelectorAll(".car-button").forEach(btn => {
+        btn.disabled = true;
+        btn.classList.add("disabled");
+    });
+}
+
 // listen race state updates
-socket.on('state:update', (currentRace) => {
-    if (!currentRace || !currentRace.drivers) return;
-    renderCarButtons(currentRace.drivers);
+socket.on('state:update', (state) => {
+    const currentRace = state.currentRace;
+
+
+    if (!currentRace || !currentRace.drivers || currentRace.drivers.length === 0 || currentRace.isEnded) {
+        container.innerHTML = '<p>No Race found.</p>';
+        currentRaceName = null;
+        return;
+    }
+
+    renderCarButtonsOnce(currentRace.drivers, currentRace.sessionName);
 
     // disable buttons if race finished
     if (currentRace.raceMode === 'Finish') {
-        document.querySelectorAll(".car-button").forEach(btn => {
-            btn.disabled = true;
-            btn.classList.add("disabled");
-        });
+        disableButtons();
     }
 });
+socket.on('race:update', (state) => {
+    const currentRace = state.currentRace;
+    if (currentRace && currentRace.raceMode === 'Finish') {
+        disableButtons();
+    }
+
+});
+
+socket.on('connect', () => {
+    socket.emit('race:requestState')
+})
