1be605b5e15dad719b9e64fa9a3af30b7a520e33
Martin Hiiesalu <hiiesalumartin@gmail.com>
2025-09-19 17:16:56 +0300

    fix for my receptionist page

--- backend/state.js
+++ backend/state.js
@@ -6,47 +6,7 @@ const stateFile = "raceState.json";
 // state.js
 export let raceTrackState = {
   //Upcoming races -> pushed from front desk(?)
-  upComingRaces: [
-    {
-      sessionName: 'race1',
-      drivers: [
-        { 
-          name: 'Milvi', 
-          carNumber: 1, 
-          fastestLap: null, 
-          currentLap: 0 },
-        { 
-          name: 'Endel', 
-          carNumber: 2, 
-          fastestLap: null, 
-          currentLap: 0 },
-      ],
-      durationSeconds: null,
-      raceMode: "",
-      isStarted : false,
-      isEnded : false,
-    },
-    {
-      sessionName: 'race2',
-      drivers: [
-        { 
-          name: 'Milvikas', 
-          carNumber: 1, 
-          fastestLap: null, 
-          currentLap: 0 },
-        { 
-          name: 'Endelhehe', 
-          carNumber: 2, 
-          fastestLap: null, 
-          currentLap: 0 },
-      ],
-      durationSeconds: null,
-      raceMode: "",
-      isStarted : false,
-      isEnded : false,
-    },
-  ],
-
+  upComingRaces: [],
   //Race Control takes the first element of upComingRaces and assignes its values to currentRace. Then removes 
   // first element of upComingRaces
   currentRace: {
